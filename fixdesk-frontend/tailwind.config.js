1d63fce179d1538913d9b6fdb33efaa4e7e6e95e
Krakukjit Krachakjai <nutpopfnaf@gmail.com>
2025-10-23 00:59:41 +0700

    style:ติดตั้งฟ้อน inter และ Noto Sans Thai สำหรับใช้งาน

--- fixdesk-frontend/tailwind.config.js
+++ fixdesk-frontend/tailwind.config.js
@@ -6,7 +6,19 @@ export default {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Inter', 'Noto Sans Thai', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', 'sans-serif'],
+      inter: ['Inter', 'Noto Sans Thai', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        primary: {
+          light: '#4db5ff',
+          DEFAULT: '#1E48D1',
+          dark: '#0d2a7a',
+        },
+      },
+    },
   },
   plugins: [
     require('flowbite/plugin')
