8f991ebb5914e2b8b00abc3f62a39a3ff61e92e3
Bet-tan <ssbackup30@gmail.com>
2025-12-04 10:08:46 +0700

    Update stock-home-view.vue
    -Update button "แจ้งซ่อม"

--- fixdesk-frontend/src/views/stock/stock-home-view.vue
+++ fixdesk-frontend/src/views/stock/stock-home-view.vue
@@ -7,9 +7,9 @@
         <h1 class="text-2xl font-bold text-gray-800">หน้าแรก</h1>
         <p class="text-gray-600">รายการของที่เบิก</p>
       </div>
-      <button class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md flex items-center">
+      <button @click="$router.push('/main/repair-request')" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md flex items-center">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/>
         </svg>
         แจ้งซ่อม
       </button>
